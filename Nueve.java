480508a55cf4b3445a5341d98748bd68941c9712
Miguel A. Gonzales <88881293+mgrmars@users.noreply.github.com>
2022-08-10 19:57:45 -0500

    Update Nueve.java

--- Nueve.java
+++ Nueve.java
@@ -1,33 +1,24 @@
+package pkg100codigoplanoejercicio9b;
+
 import java.util.Scanner;
 
-public class Nueve {
-
+public class Main {
     public static void main(String[] args) {
         /*
         Obtener el IGV (18%) de una venta
         si esta es superior a S/ 150,000.00 
         aplicar un descuento del 25 %.
          */
-
         Scanner s = new Scanner(System.in);
-
-        final double IGV = 0.18;
-        final double DESCUENTO = 0.25;
-        double venta = 0;
-        double igv = 0;
-        double descuento = 0;
-        double total = 0;
+        double venta, igv, descuento, total;
 
         System.out.println("Ingresa el valor de venta: ");
         venta = s.nextDouble();
 
-        igv = venta * IGV;
-        descuento = venta > 150000 ? venta * DESCUENTO : 0;
+        igv = venta * 18;
+        descuento = venta > 150000 ? venta * 0.25 : 0;
         total = venta - descuento + igv;
 
-        System.out.println("IGV: " + igv);
-        System.out.println("Descuento: " + descuento);
-        System.out.println("Total: " + total);
-        
+        System.out.println("IGV: " + igv + "\n" + "Descuento: " + "descuento " + "\n" + "Total: " + total);
     }
 }
